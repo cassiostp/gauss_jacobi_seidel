2b00d9401c8b093c304231e5a4f02762243de013
Cassio <cassio@localhost.localdomain>
2016-07-10 18:33:26 -0300

    Gauss_seidel corrigido

--- gauss_jacobi_seidel.py
+++ gauss_jacobi_seidel.py
@@ -16,7 +16,7 @@ def gauss_jacobi(A, b, n, e):
     x = []
     X = []
     nint = 0  # contador de iterações
-    maxint = 1000000  # número máximo de iterações
+    maxint = 100000  # número máximo de iterações
     for k in range(n):  # laço preenche o vetor X com uma possivel resposta
         X.append(1)
         x.append(0)  # no caso do vetor x, é apenas alocação de índices
@@ -40,18 +40,18 @@ def gauss_seidel(A, b, n, e):
     X = []
     x = []
     nint = 0
-    maxint = 1000000
+    maxint = 100000
     for k in range(n):
         X.append(1)
         x.append(0)
     while nint < maxint:
-        for i in range(n):
+        for i in range(0, n, 1):
             soma = 0
-            for j in range(i):
+            for j in range(0, i, 1):
                 soma += A[i][j] * x[j]
             for j in range(i+1, n, 1):
                 soma += A[i][j] * X[j]
-        x[i] = (b[i] - soma) / A[i][i]
+            x[i] = (b[i] - soma) / A[i][i]
         if abs(norma(x) - norma(X)) < e:
             return x
         else:
